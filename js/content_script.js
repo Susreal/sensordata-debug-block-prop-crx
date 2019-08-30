59adb0b4ceb9581720bea945a3b1e61d74eabf12
Susreal Zhao <zhaoxreg@163.com>
2019-08-30 13:17:26 +0800

    🎈 添加highlight
    🎈 添加highlight，高亮JSON

--- js/content_script.js
+++ js/content_script.js
@@ -1,10 +1,10 @@
 var optimize = setInterval(function(){
     let data = document.getElementsByClassName("data-json");
     for (let i=0;i<data.length;i++) {
-        let old_obj = JSON.parse(data[i].innerText)[0];    
+        //let old_obj = JSON.parse(data[i].innerText)[0];
+		let old_obj = JSON.parse($(".parse > button").eq(2*i+1).attr("data-origin"))[0];
         if (old_obj) {
-            if (old_obj.hasOwnProperty("prop")) continue;
-            
+            // if (old_obj.hasOwnProperty("prop")) continue;
             let new_obj = {};
             let prop = JSON.parse(JSON.stringify(old_obj.properties));
 
@@ -14,15 +14,19 @@ var optimize = setInterval(function(){
                     for (let j=0;j<response.data.length;j++) {
                         delete prop[response.data[j]];
                     }
+                    // new_obj.time = old_obj.time;
+                    new_obj.event = old_obj.event;
+                    new_obj.prop = JSON.parse(JSON.stringify(prop));     
+                    
+                    let new_obj_str = JSON.stringify(new_obj,null,2);//参数 null,2 可以格式化json
+                    data[i].innerHTML = "<pre><code class=\"json\">[" + new_obj_str + "]</code></pre>";
+                    
+                    //highlight json
+                    data[i].querySelectorAll('pre code').forEach((block) => {
+                        hljs.highlightBlock(block);
+					});
                 }
             });
-            // new_obj.time = old_obj.time;
-            new_obj.event = old_obj.event;
-            new_obj.prop = JSON.parse(JSON.stringify(prop));     
-            
-            let new_obj_str = JSON.stringify(new_obj);
-            // new_obj_str = new_obj_str.replace(/,/g, ",<br>");
-            data[i].innerHTML = "[" + new_obj_str + "]";
         } 
     }
 }, 500);
